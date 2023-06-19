8a81e46127d72afdc764f9f95b62506f806ec3cb
Ramon Silva Freitas <ramonsilvaf@gmail.com>
2023-06-19 16:54:24 -0300

    exercicio complementares 9 à13

--- script.js
+++ script.js
@@ -24,6 +24,38 @@ alunos.splice(index, 1)//07
 
 alunos.sort()//8
 
-console.log(alunos) //(EXERCICIO 8)
+// console.log(alunos) //(EXERCICIO 8)
 
+numero = 0
 
+for (const nome of alunos) {
+    // console.log(numero++ + " - " + nome); //(EXERCICO 9)
+}
+
+nota = [];
+
+// console.log(nota) //(EXERCICIO 10)
+
+for (i = 0; i < 15; i++) {
+    notaalunos = 9              //(EXERCICO 11)
+    nota.push(notaalunos)
+}
+
+nota = nota.map(notas => notas +1) // (EXERCICO 12)
+
+maiorQSete = nota
+maiorQSete = maiorQSete.filter(variavel => variavel > 7) // (EXERCICO 13) 
+
+boletim = alunos.map((aluno, index) => `${aluno} - Nota ${nota[index]}`)
+
+boletimOrganizado = boletim.join('/n')
+// console.log(boletim) //RESULTADO 10-13
+// console.log(boletimOrganizado) //RESULTADO 10-13
+
+const alunos = [
+    {nome:'João', nota:8}
+    {nome:'Maria', nota:5}
+    {nome:'Pedro', nota:7.5}
+    {nome:'Ana', nota:6}
+    {nome:'Carlos', nota:9}
+]
