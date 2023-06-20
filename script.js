9aa983c9fcc1a6389bb5e68be79ebafe797ea030
Ramon Silva Freitas <ramonsilvaf@gmail.com>
2023-06-20 17:21:55 -0300

    exercicios do 14 ate 19 com leves correções

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-alunos = ['joão', 'maria', 'woshinton', 'pepe', 'lucas', 'raquel', 'antonio', 'cleitin', 'onorio', 'tainara', 'Ana', 'Madalena', 'Helena', 'Juliana', 'Sandra']
+alunos = ['joão', 'Maria', 'Woshinton', 'Pepe', 'Lucas', 'Raquel', 'antonio', 'cleitin', 'onorio', 'tainara', 'Ana', 'Madalena', 'Helena', 'Juliana', 'Sandra']
 
 alunos.push('samuel', 'david', 'andré') //(EXERCIO 1)
 
@@ -14,12 +14,12 @@ alunos.sort().reverse()
 
 // console.log(alunos) //(EXERCIO 4)
 
-alunos.shift()
-alunos.pop()
+alunos.shift()//05
+alunos.pop() //06
 
 // console.log(alunos) //(EXERCICIO 5 E 6)
 
-index = alunos.indexOf(5)//07
+index = alunos.indexOf(5,1)//07
 alunos.splice(index, 1)//07
 
 alunos.sort()//8
@@ -28,7 +28,7 @@ alunos.sort()//8
 
 numero = 0
 
-for (const nome of alunos) {
+for (let numero of alunos) {
     // console.log(numero++ + " - " + nome); //(EXERCICO 9)
 }
 
@@ -36,10 +36,14 @@ nota = [];
 
 // console.log(nota) //(EXERCICIO 10)
 
-for (i = 0; i < 15; i++) {
-    notaalunos = 9              //(EXERCICO 11)
-    nota.push(notaalunos)
+for (i = 0; i < alunos.length; i++) {
+    let notaalunos = Math.random()*10    //SOFT CODE //(EXERCICIO 11)
+    nota.push(notaalunos.toFixed(1))
+
+    //HARD CODE
+    //nota.push((Math.random()*10).toFixed(1)) //(EXERCICIO 11)
 }
+// console.log(nota) //(EXERCICIO 11)
 
 nota = nota.map(notas => notas +1) // (EXERCICO 12)
 
@@ -52,10 +56,29 @@ boletimOrganizado = boletim.join('/n')
 // console.log(boletim) //RESULTADO 10-13
 // console.log(boletimOrganizado) //RESULTADO 10-13
 
-const alunos = [
-    {nome:'João', nota:8}
-    {nome:'Maria', nota:5}
-    {nome:'Pedro', nota:7.5}
-    {nome:'Ana', nota:6}
-    {nome:'Carlos', nota:9}
-]
+for(i = 0; i < alunos.length; i++) {
+    let nome = alunos [i]
+    let notas = nota [i]
+
+    // console.log('Aluno: '+ nome + " - Nota: " + notas )//(14)
+}
+
+nota.forEach(notinha => {
+    if(notinha >= 7.0) {
+        // console.log("aprovado")
+    }
+    else {
+        // console.log("Reprovado") //(15)
+    }
+});
+
+// console.log(alunos.indexOf('LUCAS'));//(EXERCICIO 16)
+
+let nerd = nota.findIndex(notas => notas > 9)
+// console.log(nerd) //(EXERCICO 17)
+
+// console.log(alunos.includes("LUCAS"))//(18)
+
+let soma = nota.reduce((acumulador, item) => acumulador += item)//(19)
+let media = soma / nota.length
+// console.log(media)
